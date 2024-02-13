5123249e75188ecfc3d9e2b5cceb88575d762362
kolom <s_kolomeetzc@ukr.net>
2024-02-13 07:24:30 +0100

    Homework first,second and three finish

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 print("Hello world")
 
 with open("initial.txt", "w") as file:
-    file.write("To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep.")
+    file.write("To be, or not to be, that is the question, "
+               "Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, "
+               "Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; "
+               "No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, "
+               "'tis a consummation Devoutly to be wish'd. To die, to sleep.")
 
 with open("initial.txt", "r") as file:
     text_from_file = file.read()
@@ -12,3 +16,13 @@ words = re.findall(r"\w{7,}", text_from_file)
 
 with open("final.txt", "w") as file:
     file.write(" ".join(words))
+
+
+with open('initial.txt', "r") as file:
+    text = file.read()
+
+restricted_word = "die"
+print(text)
+print(text.count(restricted_word))
+text = text.replace(restricted_word.lower(), "*" * len(restricted_word))
+print(text)
